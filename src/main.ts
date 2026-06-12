8fad2e219126f1d89b68bc18e9d8ffd23931c297
Braian Palacios <braianpalacios699@gmail.com>
2026-06-12 03:58:06 -0300

    fix: update test 2

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     new LoggingInterceptor(),
   );
 
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
